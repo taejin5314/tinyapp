8e0bac28da1a4fc300ffaf67cfb98147d45a1a5f
Taejin Jung <taejin5314@gmail.com>
2021-07-05 21:04:41 +0000

    Add redirection to /urls POST method

--- express_server.js
+++ express_server.js
@@ -5,11 +5,11 @@ const bodyParser = require("body-parser");
 
 function generateRandomString() {
   let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   let charactersLength = characters.length;
 
   for (let i = 0; i < 6; i++) {
-    result += characters[Math.floor(Math.random() * charactersLength)]
+    result += characters[Math.floor(Math.random() * charactersLength)];
   }
 
   return result;
@@ -40,21 +40,27 @@ app.get('/hello', (req, res) => {
 
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
-})
+});
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
-})
+});
 
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
-})
+});
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase.shortURL;
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
