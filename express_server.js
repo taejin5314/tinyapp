e0544181e560156b3c0f2bc71fcfa2650af2abc6
Taejin Jung <taejin5314@gmail.com>
2021-07-05 22:05:32 +0000

    Add update method for tinyApp

--- express_server.js
+++ express_server.js
@@ -63,6 +63,11 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect('/urls')
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
