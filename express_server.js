da84145c3e0a5e1703bc47a624941c5da7442c94
Taejin Jung <taejin5314@gmail.com>
2021-07-05 17:48:45 +0000

    install ejs and setup

--- express_server.js
+++ express_server.js
@@ -7,6 +7,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -18,6 +20,15 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
